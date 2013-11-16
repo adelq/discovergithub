6b00722ca57fdc4503a37b99e03e2ec4fdb8e6a9
Adel Qalieh <aqalieh95@gmail.com>
2013-11-15 19:55:52 -0500

    Create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Discovergithubb::Application.config.secret_key_base = '4e693bbf710aa6852554a7d8d5258bf4aaaacee8e526d2aff6f0b70baa23748555808998ce697b98e2c3d5b7f40c0f1ae72a9d5dc757a35e970088b099bb38a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Discovergithubb::Application.config.session_store :cookie_store, key: '_discovergithubb_session'
